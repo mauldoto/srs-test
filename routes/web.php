1b9612eba8e4ee2ec8848ec8bb076c7839b172a4
mauldoto <maulanaly97@gmail.com>
2021-11-15 02:30:00 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.app');
+});
+
+Route::middleware('guest-custom')->group(function () {
+    Route::get('/', function () {
+        return redirect("/data-sample");
+    });
+
+    Route::get('/login', [UserController::class, 'loginView']);
+    Route::post('/login', [UserController::class, 'login']);
+
+});
+
+Route::middleware('auth-custom')->group(function () {
+    Route::get('/data-sample', [UserController::class, 'mainView'])->name('home');
+    Route::post('/data-sample', [UserController::class, 'getSampleData']);
+    Route::post('/logout', [UserController::class, 'logout']);
 });
