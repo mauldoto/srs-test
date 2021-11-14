d078c354973bbf64632b2a94334e21123f7b0d56
mauldoto <maulanaly97@gmail.com>
2021-11-15 02:29:54 +0700

    modify main page and navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,10 +1,10 @@
 <header >
     <div class="container py-5">
         <div class="row d-flex justify-content-center align-items-center">
-            <div class="col-12 col-md-8 col-lg-6 col-xl-5 text-center">
+            <div class="col-12">
                 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="border-radius: 1rem;">
                     <div class="container-fluid d-flex justify-content-between">
-                      <a class="navbar-brand" href="#">Navbar</a>
+                      <a class="navbar-brand" href="#">SRS</a>
                       <div class="">
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
@@ -13,10 +13,11 @@
                             <ul class="navbar-nav">
                               <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                  Dropdown
+                                  {{Session::get('user')['nama']}}
                                 </a>
                                 <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                                  <li><a class="dropdown-item" href="#">Logout</a></li>
+                                    <form id="logout-form" class="hidden" action="{{ url('/logout') }}" method="POST">@csrf</form>
+                                    <li style="cursor: pointer;"><a class="dropdown-item" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a></li>
                                 </ul>
                               </li>
                             </ul>
