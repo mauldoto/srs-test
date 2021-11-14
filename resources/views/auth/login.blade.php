e4873b5b4af90db2b65c24350e2fe3b831f07b0c
mauldoto <maulanaly97@gmail.com>
2021-11-15 02:29:26 +0700

    modify login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,37 @@
-<section>
+@extends('layouts.app')
+
+@section('content')
+<section class="py-5">
     <div class="container py-5">
       <div class="row d-flex justify-content-center align-items-center">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+
+            @if ($errors->any())
+            <div class="alert alert-danger d-flex align-items-center" role="alert">
+                <div>
+                    @foreach ($errors->all() as $err)
+                        - {{ $err }} <br>
+                    @endforeach
+                </div>
+            </div>
+            @endif
+
           <div class="card shadow-2-strong" style="border-radius: 1rem;">
             <div class="card-body p-5 shadow" style="border-radius: 1rem;">
   
-              <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Email</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-              </div>
-              <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="password">
-              </div>
-  
-              <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+              <form action="{{ url('/login') }}" method="post">
+                @csrf
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email</label>
+                    <input type="email" name="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" value="{{old("email")}}">
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Password</label>
+                    <input type="password" name="password" class="form-control" id="exampleFormControlInput1" placeholder="password">
+                  </div>
+      
+                  <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+              </form>
   
             </div>
           </div>
@@ -22,3 +39,4 @@
       </div>
     </div>
   </section>
+@endsection
