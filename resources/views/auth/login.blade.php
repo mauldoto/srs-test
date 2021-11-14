248faf46be21a52833ab4daf25503635a93b4fdf
mauldoto <maulanaly97@gmail.com>
2021-11-14 19:57:42 +0700

    init commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,24 @@
+<section>
+    <div class="container py-5">
+      <div class="row d-flex justify-content-center align-items-center">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card shadow-2-strong" style="border-radius: 1rem;">
+            <div class="card-body p-5 shadow" style="border-radius: 1rem;">
+  
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="password">
+              </div>
+  
+              <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+  
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,30 @@
+<header >
+    <div class="container py-5">
+        <div class="row d-flex justify-content-center align-items-center">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5 text-center">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="border-radius: 1rem;">
+                    <div class="container-fluid d-flex justify-content-between">
+                      <a class="navbar-brand" href="#">Navbar</a>
+                      <div class="">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                          </button>
+                          <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+                            <ul class="navbar-nav">
+                              <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                  Dropdown
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                  <li><a class="dropdown-item" href="#">Logout</a></li>
+                                </ul>
+                              </li>
+                            </ul>
+                          </div>
+                      </div>
+                    </div>
+                  </nav>
+            </div>
+        </div>
+    </div>
+</header>
